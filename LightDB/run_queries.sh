8698bab0c344b68f0e0e8273a8032ad288fc659f
xarey0629 <xarey0629@gmail.com>
2024-03-06 23:06:18 +0000

    1. Pass all tests. Add a run bash file.

--- LightDB/run_queries.sh
+++ LightDB/run_queries.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# 循环遍历 1 到 12
 for i in {1..12}
 do
-    # 构建命令
     command="java -jar target/lightdb-1.0.0-jar-with-dependencies.jar samples/db samples/input/query${i}.sql samples/output/query${i}.csv"
-    
-    # 执行命令
     echo "Running command: $command"
     $command
 done
